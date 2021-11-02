7ec0fad52d7e3802c0794d3c9098c460bb4377f6
arturo.opsetmoen.amador <AMAART@banenor.no>
2021-11-02 21:38:33 +0100

    fixces run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pylint home/arturo_docker/clean_churn/tests/churn_script_logging_and_test.py > /home/arturo_docker/logs/pylint.log
+pylint home/arturo_docker/clean_churn/tests/churn_script_logging_and_tests.py > /home/arturo_docker/logs/pylint.log
