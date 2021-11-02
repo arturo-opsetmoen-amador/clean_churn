be1aea81f3ae516794cbc2d3662a99c519997a7c
arturo.opsetmoen.amador <AMAART@banenor.no>
2021-11-02 21:04:50 +0100

    run.sh fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pylint home/arturo_docker/home/clean_churn/tests/churn_script_logging_and_test.py
+pylint home/arturo_docker/clean_churn/tests/churn_script_logging_and_test.py > pylint.log
